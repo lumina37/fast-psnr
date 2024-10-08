a6963de2405726e21b4e9485e1be2ba9821fe1d9
lumina37 <starry.qvq@gmail.com>
2024-10-08 17:14:55 +0800

    feat: nodiscard marker

--- src/include/psnr/helper/constexpr/math.hpp
+++ src/include/psnr/helper/constexpr/math.hpp
@@ -7,7 +7,7 @@ namespace psnr::_hp {
 
 template <size_t to, typename T>
     requires std::is_integral_v<T> && (to % 2 == 0)
-static constexpr inline T round_to(T v)
+[[nodiscard]] static constexpr inline T round_to(T v)
 {
     constexpr T half_to = to >> 1;
     return (v + half_to) / to * to;
@@ -15,14 +15,14 @@ static constexpr inline T round_to(T v)
 
 template <typename Tv>
     requires std::is_unsigned_v<Tv>
-static constexpr inline Tv log2(const Tv v)
+[[nodiscard]] static constexpr inline Tv log2(const Tv v)
 {
     return std::bit_width(v) - 1;
 }
 
 template <size_t base, typename T>
     requires std::is_integral_v<T> && (base % 2 == 0)
-static constexpr inline bool is_mul_of(T v)
+[[nodiscard]] static constexpr inline bool is_mul_of(T v)
 {
     return (v & (1 << log2(base))) == 0;
 };
@@ -35,21 +35,21 @@ template <typename T>
 
 template <typename Tv>
     requires std::is_unsigned_v<Tv>
-static constexpr inline bool is_pow_of_2(const Tv v)
+[[nodiscard]] static constexpr inline bool is_pow_of_2(const Tv v)
 {
     return (v & (v - 1)) == 0;
 }
 
 template <size_t to, typename T>
     requires std::is_integral_v<T> && (is_pow_of_2(to))
-static constexpr inline T align_up(T v)
+[[nodiscard]] static constexpr inline T align_up(T v)
 {
     return (v + (to - 1)) & ((~to) + 1);
 };
 
 template <size_t to, typename T>
     requires std::is_integral_v<T> && (is_pow_of_2(to))
-static constexpr inline T align_down(T v)
+[[nodiscard]] static constexpr inline T align_down(T v)
 {
     return v & ((~to) + 1);
 };
