9eccc0e1cc4152ca30ff2b6281b0a94f6c010237
lumina37 <starry.qvq@gmail.com>
2024-11-16 00:54:17 +0800

    feat: add mini case for disassemble

---
+++ src/bin/minicase-v4.cpp
@@ -0,0 +1,18 @@
+#include <cstddef>
+#include <iostream>
+
+#include "psnr.hpp"
+
+int main()
+{
+    const auto width = 1920;
+    const auto height = 1080;
+
+    const size_t ysize = width * height;
+    psnr::Yuv420Frame lframe{ysize};
+    psnr::Yuv420Frame rframe{ysize};
+
+    double psnr = psnr::PsnrOp<psnr::mse::v4::MseOpu8>(lframe.getY(), rframe.getY(), ysize);
+
+    std::cout << psnr << std::endl;
+}
